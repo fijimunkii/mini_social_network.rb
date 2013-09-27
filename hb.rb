daac513c0f9dc7e5aeb8efbecbc537e21c6a86b9
Harrison Powers <fijimunkii@gmail.com>
2013-09-27 15:11:50 -0400

    works with external json

--- hb.rb
+++ hb.rb
@@ -2,9 +2,13 @@ require 'pry'
 require 'pry-remote'
 require 'pry-nav'
 
+require 'json'
+
 require 'pp'
 
 LOGIN_MENU = "
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 If you want to signup, type:
 > signup
 
@@ -17,26 +21,83 @@ To quit, type:
 "
 
 MAIN_MENU = "
-If you want to list hashkickers, type:
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+To list hashkickers, type:
 > list
 
-If you want to view someone's profile, type:
+To view someone's profile, type:
 > view username
 
-If you want to post on someone's wall, type:
+To post on someone's wall, type:
 > post username
 
-If you want to log out, type:
+To log out, type:
 > logout
 
-If you want to quit, type:
+To quit, type:
 > quit
 
 "
 
-@hb_db = {}
+PROFILE_MENU = "
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-@user_status = nil
+To set your location, type:
+> location
+
+To set your interests, type:
+> interests
+
+To set your profile picture, type:
+> picture
+
+To set your relationship status, type:
+> relationship
+
+To go back, type:
+> back
+
+"
+
+PROFILE_INTERESTS_MENU = "
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+To set your favorite books, type:
+> books
+
+To set your favorite movies, type:
+> movies
+
+To set your favorite music, type:
+> music
+
+To set your favorite food, type:
+> food
+
+To go back, type:
+> back
+"
+
+PROFILE_LOCATION_MENU = "
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+To set your hometown, type:
+> hometown
+
+To set your current location, type:
+> current
+
+To go back, type:
+> back
+"
+
+def load_json_file( filename )
+	JSON.parse( IO.read(filename) )
+end
+def write_json_file( object, filename )
+	File.open(filename, 'w'){ |file| JSON.dump( object, file) }
+end
 
 def ask(string)
 	puts string
@@ -47,7 +108,7 @@ def signup
 	while true
 		puts
 		req_username = ask('What would you like your username to be?')
-		break if @hb_db.has_key?(req_username.to_sym) == false
+		break if @hb_db.has_key?(req_username) == false
 	end
 	puts
 	puts "That username is available."
@@ -58,8 +119,8 @@ def signup
 		password_2 = ask("Please re-enter your password.")
 		break if password_1 == password_2
 	end
-	@hb_db[req_username.to_sym] = Hash.new
-	@hb_db[req_username.to_sym][:password] = password_1
+	@hb_db[req_username] = Hash.new
+	@hb_db[req_username][:password] = password_1
 	puts
 	puts 'Thank you. Your account has now been created. Please proceed to login.'
 end
@@ -69,8 +130,8 @@ def login
 	username = ask('Please enter your username:')
 	password = ask('Please enter your password:')
 	## Check if valid
-	if @hb_db.has_key?(username.to_sym) == true
-		@user_status = 'logged_in' if @hb_db[username.to_sym][:password] == password
+	if @hb_db.has_key?(username) == true
+		@user_status = 'logged_in' if @hb_db[username]['password'] == password
 	end
 	puts
 	puts "You are now logged in." if @user_status == 'logged_in'
@@ -81,9 +142,16 @@ def logout
 	@user_status = nil
 	puts "You have been logged out."
 end
+
+
+@user_status = nil
+
+@hb_db = load_json_file( 'hb_db.json' )
+
 while true
 
 	until @user_status == 'logged_in'
+		%x(clear)
 		puts LOGIN_MENU
 		user_entry = gets.chomp
 		signup if user_entry == 'signup'
@@ -92,6 +160,7 @@ while true
 	end
 
 	while @user_status == 'logged_in'
+		%(clear)
 		puts MAIN_MENU
 		user_entry = gets.chomp
 		pp @hb_db.keys if user_entry == 'list'
@@ -102,4 +171,6 @@ while true
 	break if user_entry == 'quit'
 
 end
+
+write_json_file( @hb_db, 'hb_db.json')
 puts "bye."
